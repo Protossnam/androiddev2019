f66e9448686de9f7e170afc02948884c1047f34f
rinzlerr <duongquoctue@gmail.com>
2018-10-27 09:15:11 +0700

    add LoginActivity

---
+++ app/src/main/java/vn/edu/usth/moodle/LoginActivity.java
@@ -0,0 +1,18 @@
+package vn.edu.usth.moodle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
